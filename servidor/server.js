66f016279efe8637997ac566552d53c8d3daa771
KarenCan529 <karencan529@gmail.com>
2025-03-01 15:50:05 -0500

    Configura un servidor Express con CORS y dotenv, y define la ruta /api/animales

--- servidor/server.js
+++ servidor/server.js
@@ -1,1 +1,18 @@
-console.log("ola");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(express.json()); 
+app.use(cors()); 
+
+app.use('/api/animales', require('./routes/animales'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
