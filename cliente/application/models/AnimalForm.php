a2d6bbd356d36ff9cf700511fbae9b03aa5607a7
Polanco-01 <polancojonathan42@gmail.com>
2025-03-02 18:44:05 -0500

    Se agrego la funcion insertar en la tabla guia y eliminar para las tablas guia,animal y admin J Polanco

--- cliente/application/models/AnimalForm.php
+++ cliente/application/models/AnimalForm.php
@@ -11,4 +11,28 @@ class AnimalForm extends CI_Model {
             return false;
         }
     }
+
+    public function insertarGuia($data) {
+        if ($this->db->insert('guia', $data)) {
+            return true; 
+        } else {
+            log_message('error', 'Error al insertar en la tabla guia: ' . $this->db->error()['message']);
+            return false;
+        }
+    } 
+    
+    public function eliminarGuia($id_guia) {
+        $this->db->where('id_guia', $id_guia);
+        return $this->db->delete('guia'); 
+    }  
+    
+    public function eliminarAnimal($id_animal) {
+        $this->db->where('id_animal', $id_animal);
+        return $this->db->delete('animal'); 
+    } 
+
+    public function eliminarAdministrador($id_administrador) {
+        $this->db->where('id_administrador', $id_administrador);
+        return $this->db->delete('administrador'); 
+    } 
 }
