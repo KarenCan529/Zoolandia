307226fbbfbb42857cbe64bc12d13b7dc143b864
KarenCan529 <karencan529@gmail.com>
2025-03-03 20:37:07 -0500

    Se definieron las rutas de los metodos agregados anteriormente-karen

--- servidor/routes/animales.js
+++ servidor/routes/animales.js
@@ -1,8 +1,12 @@
+
 const express = require('express');
 const router = express.Router();
 const animalController = require('../controllers/animalController');
 
+// Definir rutas
 router.get('/', animalController.getAnimales);
+router.get('/clasificaciones', animalController.getClasificaciones);
+router.get('/estado-conservacion', animalController.getEstadosConservacion);
 router.get('/:id', animalController.getAnimalById);
 router.post('/', animalController.createAnimal);
 router.put('/:id', animalController.updateAnimal);
