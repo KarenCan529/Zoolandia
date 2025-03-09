1d247862c3b1f2a37da99786d20efe1d59a2a4bd
angel123455555 <angelgabrielhmhmhm@gmail.com>
2025-03-09 18:24:12 -0500

    Modificacion de archivo animales de la carpeta route. Malpica

--- servidor/routes/animales.js
+++ servidor/routes/animales.js
@@ -2,21 +2,18 @@ const express = require('express');
 const router = express.Router();
 const animalController = require('../controllers/animalController');
 const verificarJWT = require('../middlewares/verificarJWT');
-// Definir rutas
+
 router.get('/', animalController.getAnimales);
 router.get('/clasificaciones', animalController.getClasificaciones);
 router.get('/estado-conservacion', animalController.getEstadosConservacion);
 router.get('/:id', animalController.getAnimalById);
 
 router.use((req, res, next) => {
-    if (req.method === 'GET') {
-        return next();
-    }
     verificarJWT(req, res, next); 
 });
+
 
 router.post('/', animalController.createAnimal);
 router.put('/:id', animalController.updateAnimal);
 router.delete('/:id', animalController.deleteAnimal);
-
 module.exports = router;
