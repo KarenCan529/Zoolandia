57041d90771584c51fafc1623e96e390b19ca475
angel123455555 <angelgabrielhmhmhm@gmail.com>
2025-03-09 18:25:26 -0500

    Modificacion de archivo compras de la carpeta route para requerir token tambien en las rutas get. Malpica

--- servidor/routes/compras.js
+++ servidor/routes/compras.js
@@ -5,6 +5,10 @@ const verificarJWT = require('../middlewares/verificarJWT');
 
 
 
+router.use((req, res, next) => {
+    verificarJWT(req, res, next); 
+});
+
 router.get('/reservas', compraController.getReservas);
 router.get('/', compraController.getCompras);
 router.get('/boletos', compraController.getBoletos);
@@ -12,15 +16,6 @@ router.get('/paquetes', compraController.getPaquetes);
 router.get('/agradecimientos', compraController.getAgradecimientos);
 router.get('/guias', compraController.getGuias); 
 router.get('/rutas', compraController.getRutas);  
-
-router.use((req, res, next) => {
-    if (req.method === 'GET') {
-        return next();
-    }
-    verificarJWT(req, res, next); 
-});
-
-
 router.post('/reservas', compraController.createReserva);
 router.post('/', compraController.createCompra);
 router.post('/boletos', compraController.createBoleto); 
@@ -29,6 +24,7 @@ router.post('/guias', compraController.createGuia);
 router.put('/guias/:id_guia', compraController.updateGuia); 
 router.delete('/guias/:id_guia', compraController.deleteGuia);  
 router.put('/rutas/:id_ruta', compraController.updateRuta); 
+
 
 
 
