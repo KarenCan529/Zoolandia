d881ecda0c0dbb8519c7adcfea1cb8460bb56b08
Polanco-01 <polancojonathan42@gmail.com>
2025-03-27 20:01:40 -0500

    se

--- servidor/server.js
+++ servidor/server.js
@@ -4,20 +4,24 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const app = express();
-
+const app = express(); 
 
 app.use(express.json()); 
 app.use(cors()); 
 
+
+require("./swagger")(app); 
+
+app.use('/api/token', require('./routes/token'));
 app.use('/api/animales', require('./routes/animales'));
 app.use('/api/donaciones', require('./routes/donaciones'));
 app.use('/api/compras', require('./routes/compras'));
-app.use('/api/reportes', require('./routes/reportes')); // Nueva ruta para reportes
+app.use('/api/reportes', require('./routes/reportes'));
 app.use('/api/admin', require('./routes/admin'));
-app.use('/api/token', require('./routes/token'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Swagger disponible en http://localhost:${PORT}/api-docs`);
 });
+
